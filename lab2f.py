d70f75b3d908ec593a901ae11056e3db9b4dd14b
Jihan.Kariyawasam <gjikariyawasam-thira@myseneca.ca>
2024-05-23 22:27:27 -0400

    Added Lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Jihan Kariyawasam
+#Author ID: 130584220
+#Date Created: 2024/05/23
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
