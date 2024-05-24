dac3fd340b8cebaba4e11b3132fa495ea4d0d71e
Jihan.Kariyawasam <gjikariyawasam-thira@myseneca.ca>
2024-05-23 22:18:58 -0400

    Added Lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Author: Jihan Kariyawasam
+#Author ID: 130584220
+#Date Created: 2024/05/23
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
